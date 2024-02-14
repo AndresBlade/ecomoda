cfa9255034ff45ec97fb25fa0589724b4e2abc60
Andres Arrieche <andresarrieche11@gmail.com>
2024-02-13 22:17:19 -0400

    agregada funcionalidad al sidebar: cuando un menu esta abierto, los demas se cierran. si el menu abierto es clickado, se cierra

--- src/components/ui/Sidebar/Sidebar.tsx
+++ src/components/ui/Sidebar/Sidebar.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { SubMenu } from "./Submenu";
-import { SidebarData } from './Sidebar_data';
+import { SubMenu } from './Submenu';
+import { sidebarData } from './sidebarData';
 
 export const Sidebar = () => {
-  return (
-	<nav className='sidebar'>
-		<Link to="/" className='sidebar__logo'>
-			<img className='logo' src="./aguja_Logo.svg" alt="logo" />
-			<span className="header__name">Ecomoda</span>
-		</Link>
-		<ul className='sidebar__menu'>
-			{SidebarData.map((item, index) => (
-				<SubMenu key={index} item={item} />
-			))}
-		</ul>
-		<div className='sidebar__sesionButton'>
-			<a className='sesionButton'>Salir de sesión</a>
-		</div>
-	</nav>
-  );
+	const [activeMenu, setActiveMenu] = useState(-1);
+	return (
+		<nav className="sidebar">
+			<Link to="/" className="sidebar__logo">
+				<img className="logo" src="./aguja_Logo.svg" alt="logo" />
+				<span className="header__name">Ecomoda</span>
+			</Link>
+			<ul className="sidebar__menu">
+				{sidebarData.map((item, index) => (
+					<SubMenu
+						key={index}
+						item={item}
+						status={index === activeMenu}
+						setActiveMenu={setActiveMenu}
+						menuNumber={index}
+					/>
+				))}
+			</ul>
+			<div className="sidebar__sesionButton">
+				<a className="sesionButton">Salir de sesión</a>
+			</div>
+		</nav>
+	);
 };
--- src/components/ui/Sidebar/Submenu.tsx
+++ src/components/ui/Sidebar/Submenu.tsx
@@ -1,52 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface SidebarItem {
-    title: string;
-    path: string;
-    icon: JSX.Element;
-    iconClosed?: JSX.Element;
-    Subnav?: SidebarItem[];
+	title: string;
+	path: string;
+	icon: JSX.Element;
+	iconClosed?: JSX.Element;
+	Subnav?: SidebarItem[];
 }
 
-export const SubMenu: React.FC<{ item: SidebarItem }> = ({ item }) => { 
-  const [status, setStatus] = useState(Boolean)
+export const SubMenu: React.FC<{
+	item: SidebarItem;
+	status: boolean;
+	setActiveMenu: React.Dispatch<React.SetStateAction<number>>;
+	menuNumber: number;
+}> = ({ item, status, setActiveMenu, menuNumber }) => {
+	const active = status ? 'visible' : '';
+	const currentStatus = status ? 'iconOpened' : 'iconClosed';
+	const subnavStatus = status ? 'visible__subnav' : 'invisible';
 
-  const handleStatus = () => {
-    setStatus(!status)
-}
+	return (
+		<li className="sidebar__menuData">
+			<Link
+				to={item.path}
+				onClick={() =>
+					setActiveMenu(previousMenuNumber =>
+						menuNumber === previousMenuNumber ? -1 : menuNumber
+					)
+				}
+			>
+				<div className={`sidebar__details ${active}`}>
+					<span>{item.icon}</span>
+					<div className="sidebar__title">
+						<span>{item.title}</span>
+						<span className={`icon ${currentStatus}`}>
+							{item.iconClosed}
+						</span>
+					</div>
+				</div>
+			</Link>
 
-const active = status ? 'visible' : ''
-const currentStatus = status 
-? 'iconOpened' 
-: 'iconClosed'
-const subnavStatus = status 
-? 'visible__subnav' 
-: 'invisible'
-
-return (
-  <li className="sidebar__menuData">
-    <Link to={item.path} onClick={handleStatus}>
-      <div className={`sidebar__details ${active}`}>
-          <span>{item.icon}</span>
-        <div className="sidebar__title">
-          <span>{item.title}</span>
-          <span className={`icon ${currentStatus}`}>{item.iconClosed}</span>
-        </div>
-      </div>
-    </Link>
-
-    {item.Subnav && (
-      <ul className={subnavStatus}>
-        {item.Subnav.map((subItem, index) => (
-          <li className="submenu__details" key={index}>
-            <span>{subItem.icon}</span>
-            <Link to={subItem.path}>{subItem.title}</Link>
-          </li>
-        ))}
-      </ul>
-    )}
-</li>
-);
+			{item.Subnav && (
+				<ul className={subnavStatus}>
+					{item.Subnav.map((subItem, index) => (
+						<li className="submenu__details" key={index}>
+							<span>{subItem.icon}</span>
+							<Link to={subItem.path}>{subItem.title}</Link>
+						</li>
+					))}
+				</ul>
+			)}
+		</li>
+	);
 };
