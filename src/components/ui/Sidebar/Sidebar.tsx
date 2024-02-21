13c8a22d4ea6deb928543fa8013bdd0d2bc0ac5d
Cesar Martinez <cmmh200427@gmail.com>
2024-02-21 00:38:04 -0400

    sidebar mobile completada, falta corregir el boton para volver a abrirla

--- src/components/ui/Sidebar/Sidebar.tsx
+++ src/components/ui/Sidebar/Sidebar.tsx
@@ -2,15 +2,26 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { SubMenu } from './Submenu';
 import { sidebarData } from './sidebarData';
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 export const Sidebar = () => {
 	const [activeMenu, setActiveMenu] = useState(-1);
+	
+	const [openedMenu, setOpenedMenu] = useState(false)
+	const handleMenu = () => setOpenedMenu(!openedMenu)
+	const visibleMenu = openedMenu ? 'closed' : '';
+
 	return (
-		<nav className="sidebar">
-			<Link to="/" className="sidebar__logo">
-				<img className="logo__img" src="./aguja_Logo.svg" alt="logo" />
-				<span className="logo__name">Ecomoda</span>
-			</Link>
+		<nav className={`sidebar ${visibleMenu}`}>
+			<header className='sidebar__header'>
+				<Link to="/" className="sidebar__logo">
+					<img className="logo__img" src="./aguja_Logo.svg" alt="logo" />
+					<span className="logo__name">Ecomoda</span>
+				</Link>
+				<span className='sidebar__closeTag' onClick={handleMenu}>
+					<IoIosArrowRoundBack size={20}/>
+				</span>
+			</header>
 			<ul className="sidebar__menu">
 				{sidebarData.map((item, index) => (
 					<SubMenu
