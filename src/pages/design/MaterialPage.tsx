340968a1ddfb80e7a95d2eb00d8c7777b30f2d0f
iabrahamdavid <iabrahamdavid@gmail.com>
2024-02-25 00:55:01 -0400

    Pagina de Materiales se agrego: titulo, buscar, rectangulo de los materiales

--- src/pages/design/MaterialPage.tsx
+++ src/pages/design/MaterialPage.tsx
@@ -1,6 +1,157 @@
 export const MaterialsPage = () => {
 	return (
-    <p>Trabaja aca </p>
+        <body>
+            
 
+            <div>
+                
+                    <div className="texto-materiales">
+
+                        <h2>Materiales Disponibles</h2>
+                        
+                    </div>
+                    
+                    <div className="buscador-materiales">
+
+                    <input type="text" name="buscador" placeholder="Buscar"/>
+                    <button type="submit"></button>
+
+                    </div>
+
+
+                <div className="rectangulo">
+
+                    <div className="rectangulo-materiales">
+
+                        <div className="seccion">
+                            <label className="nombre">Algodon</label>
+                            <label>ID:0001</label>
+
+                        </div>
+
+                        <div className="seccion">
+                            <label className="descripcion-materiales">Descripción</label>
+                            <textarea  readOnly className="descripcion-materiales">Es suave, transpirable y cómodo, ideal para ropa de verano y ropa interior.</textarea>
+    
+                        </div>
+
+                        <div className="seccion">
+                        <label className="medida-materiales">CM | M</label> 
+                        <label className="switch">
+                            <input type="checkbox"/>
+                            <span className="slider round"></span>
+                            </label>
+                        </div>
+
+                        <div className="seccion">
+                            <button>Editar</button>
+                            <button>Eliminar</button>
+                            
+                        </div>
+                        
+
+                    </div>
+                </div>
+                <div className="rectangulo">
+
+                    <div className="rectangulo-materiales">
+
+                        <div className="seccion">
+                            <label className="nombre">Algodon</label>
+                            <label>ID:0001</label>
+
+                        </div>
+
+                        <div className="seccion">
+                            <label className="descripcion-materiales">Descripción</label>
+                            <textarea  readOnly className="descripcion-materiales">Es suave, transpirable y cómodo, ideal para ropa de verano y ropa interior.</textarea>
+    
+                        </div>
+
+                        <div className="seccion">
+                        <label className="medida-materiales">CM | M</label> 
+                        <label className="switch">
+                            <input type="checkbox"/>
+                            <span className="slider round"></span>
+                            </label>
+                        </div>
+
+                        <div className="seccion">
+                            <button>Editar</button>
+                            <button>Eliminar</button>
+                            
+                        </div>
+                        
+
+                    </div>
+                </div>
+                <div className="rectangulo">
+
+                    <div className="rectangulo-materiales">
+
+                        <div className="seccion">
+                            <label className="nombre">Algodon</label>
+                            <label>ID:0001</label>
+
+                        </div>
+
+                        <div className="seccion">
+                            <label className="descripcion-materiales">Descripción</label>
+                            <textarea  readOnly className="descripcion-materiales">Es suave, transpirable y cómodo, ideal para ropa de verano y ropa interior.</textarea>
+    
+                        </div>
+
+                        <div className="seccion">
+                        <label className="medida-materiales">CM | M</label> 
+                        <label className="switch">
+                            <input type="checkbox"/>
+                            <span className="slider round"></span>
+                            </label>
+                        </div>
+
+                        <div className="seccion">
+                            <button>Editar</button>
+                            <button>Eliminar</button>
+                            
+                        </div>
+                        
+
+                    </div>
+                </div>
+                <div className="rectangulo">
+
+                    <div className="rectangulo-materiales">
+
+                        <div className="seccion">
+                            <label className="nombre">Algodon</label>
+                            <label>ID:0001</label>
+
+                        </div>
+
+                        <div className="seccion">
+                            <label className="descripcion-materiales">Descripción</label>
+                            <textarea  readOnly className="descripcion-materiales">Es suave, transpirable y cómodo, ideal para ropa de verano y ropa interior.</textarea>
+    
+                        </div>
+
+                        <div className="seccion">
+                        <label className="medida-materiales">CM | M</label> 
+                        <label className="switch">
+                            <input type="checkbox"/>
+                            <span className="slider round"></span>
+                            </label>
+                        </div>
+
+                        <div className="seccion">
+                            <button>Editar</button>
+                            <button>Eliminar</button>
+                            
+                        </div>
+                        
+
+                    </div>
+                </div>
+            </div>
+        </body>
     );
 };
