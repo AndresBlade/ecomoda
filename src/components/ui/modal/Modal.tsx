25a9d2694a262b6b524ae938d224d3c6d54bdc2b
Andres Arrieche <andresarrieche11@gmail.com>
2024-02-25 11:51:39 -0400

    agregado componente modal

---
+++ src/components/ui/modal/Modal.tsx
@@ -0,0 +1,58 @@
+import React, { ElementRef, useCallback, useEffect, useRef } from 'react';
+import { MdClose } from 'react-icons/md';
+interface ModalProps {
+	children: React.ReactNode;
+	isOpen: boolean;
+	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const Modal = ({ children, isOpen, setIsOpen }: ModalProps) => {
+	const dialogRef = useRef<ElementRef<'dialog'>>(null);
+
+	const closeModalWithKeyboard = useCallback(
+		(e: KeyboardEvent) => {
+			console.log('aguas');
+
+			console.log(e.key);
+			if (e.key === 'Escape') {
+				e.preventDefault();
+				setIsOpen(false);
+			}
+		},
+		[setIsOpen]
+	);
+	useEffect(() => {
+		if (isOpen) {
+			dialogRef.current?.showModal();
+			document.addEventListener('keydown', closeModalWithKeyboard);
+			return;
+		}
+
+		console.log(isOpen);
+
+		console.log('cerrando');
+		dialogRef.current?.close();
+		document.removeEventListener('keydown', closeModalWithKeyboard);
+	}, [isOpen, closeModalWithKeyboard]);
+	return (
+		<>
+			<dialog
+				className="modal"
+				ref={dialogRef}
+				onKeyDown={e => {
+					e.preventDefault();
+					if (e.key === 'escape') setIsOpen(false);
+				}}
+			>
+				<button
+					className="modal__close-button"
+					autoFocus
+					onClick={() => setIsOpen(false)}
+				>
+					<MdClose />
+				</button>
+				<div className="modal__content">{children}</div>
+			</dialog>
+		</>
+	);
+};
